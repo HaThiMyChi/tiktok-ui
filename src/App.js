fde36daf2140c6b50f1d1c939731c985ea9f69a3
MyChi <mychikhtn@gmail.com>
2025-06-23 16:34:55 +0700

    Xây dựng cơ chế tải Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { publicRoutes } from '~/Routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if(route.layout === null) {
+              Layout = Fragment;
+            }
+
+            const Page = route.component;
+
+            return (
+              <Route 
+              key={index} 
+              path={route.path} 
+              element={
+                <Layout>
+                  <Page />
+                </Layout>} 
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
