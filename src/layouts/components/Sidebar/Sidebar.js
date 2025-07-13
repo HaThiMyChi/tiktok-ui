50c8e913586de7664dbdd02d7a119ae6d13171fe
MyChi <mychikhtn@gmail.com>
2025-07-13 11:41:10 +0700

    Xây dựng UI phần sidebar #1 P2

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -2,16 +2,23 @@ import classNames from "classnames/bind";
 import styles from './Sidebar.module.scss';
 import config from "~/config";
 import Menu, {MenuItem} from "./Menu";
-import {HomeIcon, FollowingIcon, LiveIcon} from "~/components/Icons";
+import {
+        HomeIcon,
+        HomeActiveIcon,
+        UserGroupIcon,
+        UserGroupActiveIcon,
+        LiveIcon,
+        LiveActiveIcon
+    } from "~/components/Icons";
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
     return <aside className={cx('wrapper')}>
        <Menu>
-            <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} />
-            <MenuItem title="Following" to={config.routes.following} icon={<FollowingIcon />} />
-            <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} />
+            <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+            <MenuItem title="Following" to={config.routes.following} icon={<UserGroupIcon />} activeIcon={<UserGroupActiveIcon/>} />
+            <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
        </Menu>
     </aside>;
 }
